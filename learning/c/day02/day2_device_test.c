a79d033636b5e71f32b92d322a27be202c2c6d1e
Prasanna Gurav <prasannagurav641@gmail.com>
2026-08-13 01:18:16 +0530

    Add C day 02 device test

---
+++ learning/c/day02/day2_device_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int C1, C2, C3, C4, C5;
+    C1 = 1234;
+    C2 = 10;
+    C3 = 7;
+    C4 = 3;
+    C5 = 27;
+    
+    printf("Device ID: %d \nTotal tests: %d \nPassed tests: %d \nFailed tests: %d \nTemperature: %d", C1, C2, C3, C4, C5);
+
+    return 0;
+}
